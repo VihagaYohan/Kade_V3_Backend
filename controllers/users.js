93600eb61e3c9e88c462644119b40ba9b4d80b1d
VihagaYohan <vihagayohan94@gmail.com>
2021-05-05 21:25:53 +0530

    Added trycatch block to POST add user endpoint

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-
 const { User, validationUser } = require("../models/User");
 
 // @desc    get all users
@@ -48,29 +47,41 @@ exports.getUser = async (req, res, next) => {
 // @route   POST/api/users/
 // @access  private
 exports.addUser = async (req, res, next) => {
-  const { error } = validationUser(req.body);
-  if (error)
-    return res.status(400).json({
-      sucess: false,
-      data: error.details[0].message,
+  try {
+    // check user input data validation
+    const { error } = validationUser(req.body);
+    if (error)
+      return res.status(400).json({
+        sucess: false,
+        data: error.details[0].message,
+      });
+
+    // check if the user already exists in the database
+    let user = await User.findOne({ email: req.body.email });
+    if (user)
+      return res
+        .status(404)
+        .json({ sucess: false, data: "User already exists" });
+
+    // hasing user password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // create and save user
+    user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+      phoneNumber: req.body.phoneNumber,
     });
 
-  let user = await User.findOne({ email: req.body.email });
-  if (user)
-    return res.status(404).json({ sucess: false, data: "User already exists" });
+    user = await user.save();
 
-  // hasing user password
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
-  user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: hashedPassword,
-    phoneNumber: req.body.phoneNumber,
-  });
-
-  user = await user.save();
-
-  res.status(200).json({ sucess: true, data: user });
+    res.status(200).json({ sucess: true, data: user });
+  } catch (error) {
+    res.status(500).json({
+      sucess: false,
+      data: `Failed : ${error.message}`,
+    });
+  }
 };
