633623bc2c3a232c61141ce37955671551240bd1
VihagaYohan <vihagayohan94@gmail.com>
2021-05-08 13:37:30 +0530

    Added auth router and user registration route - auth/register

---
+++ controllers/auth.js
@@ -0,0 +1,44 @@
+const bcrypt = require("bcryptjs");
+const { User, validationUser } = require("../models/User");
+
+exports.registerUser = async (req, res, next) => {
+  try {
+    const { name, email, password, phoneNumber } = req.body;
+
+    // check for user data validation
+    const { error } = validationUser(req.body);
+    if (error)
+      return res.status(400).json({
+        sucess: false,
+        msg: error.details[0].message,
+      });
+
+    // check if the user email address already exists
+    let user = await User.findOne({ email });
+    if (user)
+      return res.status(400).json({
+        sucess: false,
+        msg: "User email already exists",
+      });
+
+    console.log("fahs");
+    // hashing user password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // create new user
+    user = new User({ name, email, password: hashedPassword, phoneNumber });
+    user = await user.save();
+
+    res.status(200).json({
+      sucess: true,
+      data: user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      sucess: false,
+      msg: `Failed: ${error.message}`,
+    });
+  }
+};
